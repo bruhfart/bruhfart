886b97f4f05da168aec57b03ea5a34edf858a5ac
hakr14 <jwalls814@gmail.com>
2020-12-07 21:32:26 -0500

    giant ape
    giant ape

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function funky(){
 		wrong.currentTime = 0.1;
 		wrong.play();
 	}else{
+		let ape = document.getElementById("ape");
 		for (let img of document.getElementsByTagName("img")){
 			if(!img.classList.contains("keep")){
 				img.setAttribute("width", img.width);
 				img.setAttribute("src", "resources/img/funky.png");
+				img.setAttribute("onclick", "ape.pause();ape.currentTime=0;ape.play();")
 			}
 		}
 		funk = true;
